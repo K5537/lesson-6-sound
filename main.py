3d37909724c841a636b4c9a4f1ca6cfd24a92f6c
K5537 <72745925+K5537@users.noreply.github.com>
2021-01-14 13:25:28 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,12 @@
+#while True:
+ #   if input.sound_level() >= 150:
+  #      light.show_animation(light.rainbowAnimation, 500)
+   # else:   
+    #    light.clear()
+#while True:
+    #if input.light_level() >= 20:
+        #music.play_tone(Note.C, BeatFraction.Half)
+    #else:
+        #light.clear()
+while True:
+    music.play_melody("E B C5 A B G A F ", 120)
---
+++ main.ts
@@ -0,0 +1,13 @@
+// while True:
+//    if input.sound_level() >= 150:
+//       light.show_animation(light.rainbowAnimation, 500)
+//  else:   
+//     light.clear()
+// while True:
+// if input.light_level() >= 20:
+// music.play_tone(Note.C, BeatFraction.Half)
+// else:
+// light.clear()
+while (true) {
+    music.playMelody("E B C5 A B G A F ", 120)
+}
